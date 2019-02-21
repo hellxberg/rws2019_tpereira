1f1642363c3c3869da354ea7740a2aa0aad500a0
Hellxberg <tiagotsstss@hotmail.com>
2019-02-21 10:56:29 +0000

    dasfasdf

--- player_tpereira/src/player_tpereira.cpp
+++ player_tpereira/src/player_tpereira.cpp
@@ -1,6 +1,22 @@
+#include <ros/ros.h>
 #include <iostream>
 
-int main(){
-    std::cout << "Hello world" << std::endl;
-    return 1;
+const int MY_VAR = 2;
+
+int doSomething()
+{
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "player_tpereira");
+  ros::NodeHandle n;
+
+  for (int i = 0; i < 10; i++)
+  {
+    std::cout << i << std::endl;
+  }
+
+  return 1;
 }
